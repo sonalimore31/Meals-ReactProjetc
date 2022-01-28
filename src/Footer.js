74876131ee001a56bad57a8204b8adc032fe06ae
sonali <sonalimore773@gmail.com>
2022-01-28 15:57:51 +0530

    ReactMeals Project Done

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <div style={{ backgroundColor: "brown" }}>
+      <p style={{ color: "white" }}>Designed and Developed</p>
+      <p style={{ color: "white" }}>K.Sathyaprakash Reddy</p>
+    </div>
+  )
+}
+
+export default Footer
